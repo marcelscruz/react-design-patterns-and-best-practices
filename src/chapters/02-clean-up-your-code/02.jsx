aa000056a52ae2765f12e20fb43270aea1240778
Marcel Cruz <marcelcruz@live.com>
2018-09-24 07:42:16 +0100

    add react project

---
+++ src/chapters/02-clean-up-your-code/02.jsx
@@ -0,0 +1,38 @@
+const myDiv = (
+  <div>
+    <a href="https://facebook.github.io/react/">Click me!</a>
+  </div>
+)
+
+// Translates to
+
+React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'a',
+    { href: 'https://facebook.github.io/react/' },
+    'Click me!',
+  ),
+)
+
+// Helper function
+
+canShowSecretData() {
+  const { dataIsReady, isAdmin, userHasPermissions } = this.props 
+  return dataIsReady && (isAdmin || userHasPermissions) 
+  } 
+  
+  <div> 
+    {this.canShowSecretData() && <SecretData />} 
+  </div>
+
+// Getter
+get canShowSecretData() { 
+  const { dataIsReady, isAdmin, userHasPermissions } = this.props 
+  return dataIsReady && (isAdmin || userHasPermissions) 
+  } 
+  
+  <div> 
+  {this.canShowSecretData && <SecretData />} 
+  </div> 
